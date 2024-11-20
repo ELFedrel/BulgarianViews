9cb9981271ee7e2866b237c1f46501a2a7d8e4d2
ELFedrel <ivodzhermanski@gmail.com>
2024-11-20 22:59:43 +0300

    I make Edit view model

---
+++ BulgarianViews.Web.ViewModels/LocationPost/LocationPostEditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulgarianViews.Web.ViewModels.LocationPost
+{
+    using static BulgarianViews.Common.EntityConstants.LocationPost;
+    public class LocationPostEditViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MinLength(TitleMinLength)]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        [MinLength(DescriptionMinLength)]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; } = null!;
+
+        public string? ExistingPhotoURL { get; set; }
+
+        public IFormFile? NewPhoto { get; set; }
+
+        [Required]
+        public Guid TagId { get; set; }
+
+      
+    }
+}
