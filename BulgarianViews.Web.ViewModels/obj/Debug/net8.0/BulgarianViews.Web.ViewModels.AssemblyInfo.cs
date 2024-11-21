088f7146df9f7115a3d37948537ceb6e5e33ec5e
ELFedrel <ivodzhermanski@gmail.com>
2024-11-21 15:18:53 +0300

    add my favorites controller

--- BulgarianViews.Web.ViewModels/obj/Debug/net8.0/BulgarianViews.Web.ViewModels.AssemblyInfo.cs
+++ BulgarianViews.Web.ViewModels/obj/Debug/net8.0/BulgarianViews.Web.ViewModels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulgarianViews.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30aa46c12d453494fca280341491bd2c64cba34a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulgarianViews.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulgarianViews.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BulgarianViews/Controllers/MyFavoritesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulgarianViews.Controllers
+{
+    public class MyFavoritesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
